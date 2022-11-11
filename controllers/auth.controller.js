1967268bc4502f4c515233b70afd011b77b373a7
irfannudin ihsan <irfannudinihsan56@gmail.com>
2022-11-11 10:08:53 +0700

    register & login 2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,40 +4,56 @@ const jwt = require("jsonwebtoken");
 
 module.exports = {
   registerAuth: async (req, res) => {
-    const data = req.body;
+    try {
+      const data = req.body;
 
-    passwordHash = bcrypt.hashSync(req.body.password, 8);
-    data.password = passwordHash;
-    const user = await new User(data);
-    user.save();
+      passwordHash = bcrypt.hashSync(req.body.password, 8);
+      data.password = passwordHash;
+      const user = await new User(data);
+      user.save();
 
-    res.json({
-      message: "data done created",
-    });
+      res.json({
+        message: "data has been created",
+      });
+    } catch (error) {
+      console.log(error);
+    }
   },
 
   loginAuth: async (req, res) => {
-   const data = req.body;
-    const user = await User.findOne({ email: data.email });
-
+    try {
+      const data = await req.body;
+      const user = await User.findOne({ email: data.email });
+      if (user) {
         const cekPassword = bcrypt.compareSync(data.password, user.password);
 
-        if(cekPassword) {
+        if (cekPassword) {
+          const token = jwt.sign(
+            {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+            },
+            "qwerty",
+            { expiresIn: "1d" }
+          );
 
-            const token = jwt.sign(
-                {
-                  id: user.id,
-                  name: user.name,
-                  email: user.email,
-                },
-                "asdfiuiu",
-                { expiresIn: "1d" }
-              );
-        
-              res.json({
-                message: "Login success",
-                token: token,
-              });
+          res.json({
+            message: "Login success",
+            token: token,
+          });
+        } else {
+          res.json({
+            message: "Email or Password wrong",
+          });
+        }
+      } else {
+        res.json({
+          message: "Email or Password wrong",
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
   },
 };
